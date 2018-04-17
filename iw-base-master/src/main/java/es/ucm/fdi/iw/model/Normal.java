5dee553291187af84ea789933ea5b32b9a964f0a
Milly <millydack@gmail.com>
2018-04-17 14:30:23 +0200

    Cambios de bd

--- iw-base-master/src/main/java/es/ucm/fdi/iw/model/Normal.java
+++ iw-base-master/src/main/java/es/ucm/fdi/iw/model/Normal.java
@@ -1,18 +1,9 @@
 package es.ucm.fdi.iw.model;
 
 public class Normal extends User {
-	private String dni;
 	
 	public Normal() {
 		// TODO Auto-generated constructor stub
-	}
-
-	public String getDni() {
-		return dni;
-	}
-
-	public void setDni(String dni) {
-		this.dni = dni;
 	}
 
 }
