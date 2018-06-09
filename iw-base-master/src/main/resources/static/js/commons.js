e51cd4adbf8e4b8ca36cc9cecc444d6d8d0d6b9b
millydack <millydack@gmail.com>
2018-06-09 11:45:52 +0200

    Reservas ya sí

--- iw-base-master/src/main/resources/static/js/commons.js
+++ iw-base-master/src/main/resources/static/js/commons.js
@@ -90,23 +90,47 @@ function horasDisponibles(f, h){
         url: "/reserve/booking",
         data: {date: f},
         success: function(data, statusText, jqXHR){
-        	var cad = '<div class="celda-horario">';
-        	data.forEach(element => {
-        		var hour = element.hour;
-        		
-        		if(element.reserved == 1){
-        			cad += '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="' + hour + '" id="franja-' + hour + '-' + (hour + 1) + '-' + c + '" disabled><label class="inactive" for="franja-' + hour + '-' + (hour + 1) + '-' + c + '"> ' + hour + ':00 — ' + (hour + 1) + ':00</label></div>';
-        		}else{
-        			cad += '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="' + hour + '" id="franja-' + hour + '-' + (hour + 1) + '-' + c + '"><label for="franja-' + hour + '-' + (hour + 1) + '-' + c + '"> ' + hour + ':00 — ' + (hour + 1) + ':00</label></div>';
-        		}
-        		
-        		if(hour == 11 ||hour == 14 || hour == 17){
-        			cad += '</div><div class="celda-horario">';
-        		}
-        		
-        	});  
-        	cad += '</div>';
-        	$(h).append(cad);
+        	if(data.length == 0){
+        		$(h).append(
+        	            '<div class="celda-horario">' +
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="9" id="franja-9-10-' + c + '"> <label for="franja-9-10-' + c + '"> 09:00 — 10:00</label></div>' + 
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="10" id="franja-10-11-' + c + '"> <label for="franja-10-11-' + c + '"> 10:00 — 11:00</label></div>' + 
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="11" id="franja-11-12-' + c + '"> <label for="franja-11-12-' + c + '"> 11:00 — 12:00</label></div>' + 
+        	            '</div>' + 
+        	            '<div class="celda-horario">' + 
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="12" id="franja-12-13-' + c + '"> <label for="franja-12-13-' + c + '"> 12:00 — 13:00</label></div>' + 
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="13" id="franja-13-14-' + c + '"> <label for="franja-13-14-' + c + '"> 13:00 — 14:00</label></div>' + 
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="14" id="franja-14-15-' + c + '"> <label for="franja-14-15-' + c + '"> 14:00 — 15:00</label></div>' + 
+        	            '</div>' + 
+        	            '<div class="celda-horario">' + 
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="15" id="franja-15-16-' + c + '"> <label for="franja-15-16-' + c + '"> 15:00 — 16:00</label></div>' + 
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="16" id="franja-16-17-' + c + '"> <label for="franja-16-17-' + c + '"> 16:00 — 17:00</label></div>' + 
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="17" id="franja-17-18-' + c + '"> <label for="franja-17-18-' + c + '"> 17:00 — 18:00</label></div>' + 
+        	            '</div>' + 
+        	            '<div class="celda-horario">' + 
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="18" id="franja-18-19-' + c + '"> <label for="franja-18-19-' + c + '"> 18:00 — 19:00</label></div>' + 
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="19" id="franja-19-20-' + c + '"> <label for="franja-19-20-' + c + '"> 19:00 — 20:00</label></div>' + 
+        	                '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="20" id="franja-20-21-' + c + '"> <label for="franja-20-21-' + c + '"> 20:00 — 21:00</label></div>' + 
+        	            '</div>');
+        	}else{
+	        	var cad = '<div class="celda-horario">';
+	        	data.forEach(element => {
+	        		var hour = element.hour;
+	        		
+	        		if(element.reserved == 1){
+	        			cad += '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="' + hour + '" id="franja-' + hour + '-' + (hour + 1) + '-' + c + '" disabled><label class="inactive" for="franja-' + hour + '-' + (hour + 1) + '-' + c + '"> ' + hour + ':00 — ' + (hour + 1) + ':00</label></div>';
+	        		}else{
+	        			cad += '<div><input type="checkbox" name="franja-horaria" class="franja-' + c + '" value="' + hour + '" id="franja-' + hour + '-' + (hour + 1) + '-' + c + '"><label for="franja-' + hour + '-' + (hour + 1) + '-' + c + '"> ' + hour + ':00 — ' + (hour + 1) + ':00</label></div>';
+	        		}
+	        		
+	        		if(hour == 11 ||hour == 14 || hour == 17){
+	        			cad += '</div><div class="celda-horario">';
+	        		}
+	        		
+	        	});  
+	        	cad += '</div>';
+	        	$(h).append(cad);
+        	}
         	countHours();
         },
         error: function(jqXHR, statusText, errorThrown) {
